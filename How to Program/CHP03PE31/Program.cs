cfdff44985d7153de074a7fb652235569f3c7f5c
Kevin Guan <kevguan@yahoo.com>
2017-06-07 21:40:21 -0400

    Completed Chapter 03

--- How to Program/CHP03PE31/Program.cs
+++ How to Program/CHP03PE31/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 
-/*
+/**
  * 3.31 (Body Mass Index Calculator) 
  * We introduced the body mass index (BMI) calculator in Exercise 1.29. The formulas for calculating the BMI are
  * BMI = (weightInPounds x 703) / (heightInInches x heightInInches)
  * BMI = (weightInKilograms) / (heightInMeters x heightInMeters)
  * Create a BMI calculator app that reads the user’s weight in pounds and height in inches 
- * (or, if you prefer, the user’s weight in kilograms and height in meters), then calculates and displays the user’s body mass index. 
- * The app should also display the following information from the Department of Health and Human Services/National Institutes of Health 
- * so the user can evaluate his/her BMI:
- * BMI VALUES
+ * (or, if you prefer, the user’s weight in kilograms and height in meters), then calculates and displays the user’s 
+ * body mass index. The app should also display the following information from the Department of Health and Human 
+ * Services/National Institutes of Health so the user can evaluate his/her BMI:
  * Underweight: less than 18.5
  * Normal: between 18.5 and 24.9 
  * Overweight: between 25 and 29.9
@@ -21,25 +20,19 @@ namespace CHP03PE31
     {
         static void Main(string[] args)
         {
-            int weightInPounds,
-                heightInInches,
-                bmi;
-
-            Console.Write("Enter your weight in pounds: ");
-            weightInPounds = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter your weight in pound: ");
+            int weightInPounds = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter your height in inches: ");
-            heightInInches = Convert.ToInt16(Console.ReadLine());
+            int heightInInches = Convert.ToInt32(Console.ReadLine());
+            int bmi = (int) ((weightInPounds * 703) / (Math.Pow(heightInInches, 2)));
 
-            bmi = (weightInPounds * 703) / (heightInInches * heightInInches);
+            Console.WriteLine("\nBMI range:" +
+                "\nUnderweight: less than 18.5" +
+                "\nNormal: between 18.5 and 24.9" +
+                "\nOverweight: between 25 and 29.9" +
+                "\nObese: 30 or greater");
 
-            if (bmi < 18.5)
-                Console.WriteLine("Your bmi is {0}: underweight!", bmi);
-            else if (bmi < 24.9)
-                Console.WriteLine("Your bmi is {0}: normal!", bmi);
-            else if (bmi < 29.9)
-                Console.WriteLine("Your bmi is {0}: overweight!", bmi);
-            else
-                Console.WriteLine("Your bmi is {0}: obese!", bmi);
+            Console.WriteLine("Your bmi is {0}.", bmi);
         }
     }
 }
--- How to Program/CHP03PE32/Program.cs
+++ How to Program/CHP03PE32/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-/**
+/*
  * 3.32 (Car-Pool Savings Calculator) 
- * Research several car-pooling websites. Create an app that calculates your daily driving cost, 
- * so that you can estimate how much money could be saved by car pooling, which also has other advantages 
- * such as reducing carbon emissions and reducing traffic congestion. The app should input the following information 
- * and display the user’s cost per day of driving to work: 
+ * Research several car-pooling websites. Create an app that calculates your daily driving cost, so that you can 
+ * estimate how much money could be saved by car pooling, which also has other advantages such as reducing carbon 
+ * emissions and reducing traffic congestion. The app should input the following information and display the user’s 
+ * cost per day of driving to work: 
  * a) Total miles driven per day.
  * b) Cost per gallon of gasoline (in cents).
  * c) Average miles per gallon.
@@ -22,31 +18,19 @@ namespace CHP03PE32
     {
         static void Main(string[] args)
         {
-            int milesDrivenPerDay,
-                costPerGal,
-                averageMilesPerGal,
-                parkingFee,
-                tollsPerDay,
-                totalCost,
-                numOfCarPoolers;
+            Console.Write("Enter the total miles driven per day: ");
+            int totalMilesPerDay = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the cost per gallon of gasoline (in cents): ");
+            int costPerGallon = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the average miles per gallon: ");
+            int averageMilesPerGallon = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the parking fees per day (in cents): ");
+            int parkingFee = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the tolls per day (in cents): ");
+            int tollsPerDay = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter miles driven per day: ");
-            milesDrivenPerDay = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter cost per gallon (in cents): ");
-            costPerGal = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter average miles per gallon: ");
-            averageMilesPerGal = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter parking fee (in cents): ");
-            parkingFee = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter tolls per day (in cents): ");
-            tollsPerDay = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter number of carpoolers: ");
-            numOfCarPoolers = Convert.ToInt16(Console.ReadLine());
-
-            totalCost = ((costPerGal * (milesDrivenPerDay / averageMilesPerGal) / 100) + tollsPerDay + parkingFee) / 100;
-
-            Console.WriteLine("It cost you ${0} a day!\nWith {1} carpoolers, it will cost ${2}.", totalCost, numOfCarPoolers, (totalCost / (numOfCarPoolers + 1)));
-
+            int totalCost = (((costPerGallon * (totalMilesPerDay / averageMilesPerGallon)) / 100) + parkingFee + tollsPerDay) / 100;
+            Console.WriteLine("Total cost per day is {0}.", totalCost);
         }
     }
 }
