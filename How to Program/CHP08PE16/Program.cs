4e5e758c2e439717f0bf011d79f2db8dc51b3b7d
Kevin Guan <guankev@gmail.com>
2017-10-04 19:26:07 -0500

    Completed CHP08 PE 16

--- How to Program/CHP08PE16/Program.cs
+++ How to Program/CHP08PE16/Program.cs
@@ -11,45 +11,38 @@ namespace CHP08PE16
     {
         static void Main(string[] args)
         {
-            int arraySize;
-            double[] typeDoubleArray;
-
             if (args.Length == 0)
             {
-                Console.WriteLine("No command line arguments found! Setting default size to 10..");
-                arraySize = 10;
-            } 
+                Console.WriteLine("Command-Line arguments is empty!");
+                Environment.Exit(0);
+            }
             else
             {
-                Console.WriteLine("Setting array size to {0}.", args[0]);
-                arraySize = int.Parse(args[0]);
-            }
+                Console.WriteLine("Command-Line arguments found, proceeding to evaulating values....");
 
-            typeDoubleArray = new double[arraySize];
+                double sum = 0;
+                int counter = 0;
 
-            Console.WriteLine("Initializing type double array values...");
+                foreach (string number in args)
+                {
+                    if (counter++ < (args.Length - 1))
+                        Console.Write(number + " + ");
+                    else
+                        Console.Write(number + " = ");
 
-            for (int arrayIndex = 0; arrayIndex < typeDoubleArray.Length; arrayIndex++)
-            {
-                Console.Write("Enter value {0}: ", arrayIndex + 1);
-                typeDoubleArray[arrayIndex] = Convert.ToDouble(Console.ReadLine());
-            }
+                    try
+                    {
+                        sum += Convert.ToDouble(number);
+                    }
+                    catch (FormatException fe)
+                    {
+                        Console.WriteLine("SOMEONE DIDNT PUT IN DOUBLE VALUES INSIDE THE COMMAND LINE ARGUMENTS!!!!!");
+                        Console.WriteLine(fe.Message);
+                    }
+                }
 
-            Console.WriteLine("Summing up array values...");
-            double sum = 0;
-            int counter = 0; 
-
-            foreach (double values in typeDoubleArray)
-            {
-                sum += values;
-
-                Console.Write(values);
-
-                if (counter++ != typeDoubleArray.Length - 1)
-                    Console.Write(" + ");
-            }
-
-            Console.WriteLine(" = {0}", sum);
+                Console.WriteLine(sum);
+            }            
         }
     }
 }
